7348b68c6a0e69220151fb44dd695a2c78d36e8e
Yangnyeon <tmdgurdla324@naver.com>
2022-06-29 14:44:41 +0900

    제작중..

---
+++ app/src/main/java/com/example/what_should_i_order/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.what_should_i_order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
