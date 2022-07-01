935fe5a7902531ec93c2538e815e4e88aef74093
Yangnyeon <tmdgurdla324@naver.com>
2022-07-01 23:25:16 +0900

    제작중..

---
+++ app/src/main/java/com/example/what_should_i_order/Worldcup/Food_Data.kt
@@ -0,0 +1,3 @@
+package com.example.what_should_i_order.Worldcup
+
+class Food_Data(val Food_Image : Int, val Food_text : String)
--- app/src/main/java/com/example/what_should_i_order/Worldcup/Worldcup.kt
+++ app/src/main/java/com/example/what_should_i_order/Worldcup/Worldcup.kt
@@ -17,17 +17,17 @@ import kotlin.math.sign
 
 class Worldcup : AppCompatActivity() {
 
-    var food_1Round: ArrayList<String> = ArrayList()
+    var food_1Round: ArrayList<Food_Data> = ArrayList()
 
-    var food_2Round: ArrayList<String> = ArrayList()
+    var food_2Round: ArrayList<Food_Data> = ArrayList()
 
-    var food_3Round: ArrayList<String> = ArrayList()
+    var food_3Round: ArrayList<Food_Data> = ArrayList()
 
-    var food_4Round: ArrayList<String> = ArrayList()
+    var food_4Round: ArrayList<Food_Data> = ArrayList()
 
-    var food_Final: ArrayList<String> = ArrayList()
+    var food_Final: ArrayList<Food_Data> = ArrayList()
 
-    var count = 0
+    var count = -2
 
     var count_2Round = 0
 
@@ -35,9 +35,9 @@ class Worldcup : AppCompatActivity() {
 
     var count_4Round = 0
 
+    var game_count = 0
 
-    var game_count = 1
-
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_worldcup)
@@ -50,6 +50,7 @@ class Worldcup : AppCompatActivity() {
             food_1Round.add(i.toString())
         }*/
 
+        /*
         food_1Round.add("곱창")
         food_1Round.add("보쌈")
         food_1Round.add("짜장면")
@@ -58,170 +59,295 @@ class Worldcup : AppCompatActivity() {
 
         for (i in 0..26) {
          food_1Round.add(i.toString())
-     }
+        }*/
+
+        food_1Round.apply {
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+            add(Food_Data(Food_Image = R.drawable.tteokbokki,Food_text = "떡볶이"))
+            add(Food_Data(Food_Image = R.drawable.ramen,Food_text = "라면"))
+        }
 
         food_1Round.shuffle()
 
-        left_food.text = food_1Round[count]!!
-        right_food.text = food_1Round[count + 1]!!
 
 
-        if(game_count == 0) {
-            left_card.setOnClickListener {
-                food_2Round.add(left_food.text.toString())
-                game_count++
-            }
-            right_card.setOnClickListener {
-                food_2Round.add(right_food.text.toString())
-                game_count++
-            }
-        }
 
-        food_2Round.add(left_food.text.toString())
+        game_count_text.text = "$game_count / 16\n" + food_2Round.size
 
-        game_count_text.text = "$game_count / 16"
+        left_food.text = "배달 이상형"
 
+        right_food.text = "월드컵 시작"
 
         left_card.setOnClickListener {
             count += 2
             game_count++
-            if (game_count <= 16) {
+            if (game_count <= 16 && game_count >= 1) {
 
-                left_food.text = food_1Round[count]!!
-                right_food.text = food_1Round[count + 1]!!
+                left_food.text = food_1Round[count].Food_text.toString()!!
+                right_food.text = food_1Round[count + 1].Food_text.toString()!!
 
-                game_count_text.text = "$game_count / 16"
+                left_iamge.setImageResource(food_1Round[count].Food_Image)
+                right_iamge.setImageResource(food_1Round[count + 1].Food_Image)
 
-                food_2Round.add(left_food.text.toString())
+                game_count_text.text = "$game_count / 16" + "2라운드 추가된거 " + food_2Round.size
+
+                food_2Round.apply {
+                    add(Food_Data(Food_Image = food_1Round[count].Food_Image, Food_text = left_food.text.toString()))
+                }
 
                 food_2Round.shuffle()
-            } else if(game_count <= 24) {
+            } else if (game_count <= 24 && game_count >= 2) {
 
                 count_2Round += 2
 
-                left_food.text = food_2Round[count_2Round - 2]
-                right_food.text = food_2Round[count_2Round - 1]
+                left_food.text = food_2Round[count_2Round - 2].Food_text.toString()!!
+                right_food.text = food_2Round[count_2Round - 1].Food_text.toString()!!
+
+                left_iamge.setImageResource(food_2Round[count_2Round - 2].Food_Image)
+                right_iamge.setImageResource(food_2Round[count_2Round - 1].Food_Image)
 
                 game_count_text.text =
-                    (game_count - 16.toInt()).toString() + " / 8\n" + food_2Round.toString() + food_2Round.size + "\n" + "3라운드 추가된거 " + food_3Round.toString() + food_3Round.size +    game_count.toString()
+                    (game_count - 16.toInt()).toString() + " / 8\n" + "3라운드 추가된거 " + food_3Round.size + game_count.toString()
 
                 round.text = "16강"
 
-                food_3Round.add(left_food.text.toString())
+                food_3Round.apply {
+                    add(
+                        Food_Data(
+                            Food_Image = food_1Round[count_2Round - 2].Food_Image,
+                            Food_text = left_food.text.toString()
+                        )
+                    )
+                }
 
 
                 food_3Round.shuffle()
 
-            } else if(game_count <= 28) {
+            } else if (game_count <= 28 && game_count >= 2) {
 
                 count_3Round += 2
 
-                left_food.text = food_3Round[count_3Round - 2]
-                right_food.text = food_3Round[count_3Round - 1]
+                left_food.text = food_3Round[count_3Round - 2].Food_text.toString()!!
+                right_food.text = food_3Round[count_3Round - 1].Food_text.toString()!!
 
-                game_count_text.text = (game_count - 24.toInt()).toString() + " / 4" + food_3Round.toString() + food_3Round.size + "\n" + "4강에 추가된거" + food_4Round.toString() + food_4Round.size + "게임 카운트 " + game_count.toString()
+                left_iamge.setImageResource(food_3Round[count_3Round - 2].Food_Image)
+                right_iamge.setImageResource(food_3Round[count_3Round - 1].Food_Image)
+
+
+                game_count_text.text = (game_count - 24.toInt()).toString() + " / 4" + "4라운드 추가된거 " + food_4Round.size + game_count.toString()
                 round.text = "8강"
 
-                food_4Round.add(left_food.text.toString())
+                food_4Round.apply {
+                    add(
+                        Food_Data(
+                            Food_Image = food_3Round[count_3Round - 2].Food_Image,
+                            Food_text = left_food.text.toString()
+                        )
+                    )
+                }
 
                 food_4Round.shuffle()
 
-            } else if(game_count <= 30) {
+            } else if (game_count <= 30 && game_count >= 2) {
 
                 count_4Round += 2
 
-                left_food.text = food_4Round[count_4Round - 2]
-                right_food.text = food_4Round[count_4Round - 1]
+                //
+                left_food.text = food_4Round[count_4Round - 2].Food_text.toString()!!
+                right_food.text = food_4Round[count_4Round - 1].Food_text.toString()!!
 
-                game_count_text.text = (game_count - 28.toInt()).toString() + " / 4" + food_4Round.toString() + food_4Round.size + "\n" + "결승에 추가된거" + food_Final.toString() + food_Final.size + "게임 카운트 " + game_count.toString()
+                left_iamge.setImageResource(food_4Round[count_4Round - 2].Food_Image)
+                right_iamge.setImageResource(food_4Round[count_4Round - 1].Food_Image)
+
+                //
+
+
+                game_count_text.text = (game_count - 28.toInt()).toString() + " / 2" + "결승 추가된거 " + food_Final.size + game_count.toString()
                 round.text = "4강"
 
-                food_Final.add(left_food.text.toString())
+                food_Final.apply {
+                    add(
+                        Food_Data(
+                            Food_Image = food_4Round[count_4Round - 2].Food_Image,
+                            Food_text = left_food.text.toString()
+                        )
+                    )
+                }
 
                 food_Final.shuffle()
-            } else if(game_count <= 31) {
+            } else if (game_count <= 31 && game_count >= 1) {
                 round.text = "결승전 \n"
 
-                game_count_text.text = (game_count - 28.toInt()).toString() + " / 4" + food_Final.toString() + food_Final.size
+                game_count_text.text = (game_count - 28.toInt()).toString() + " / 4"
 
-                left_food.text = food_Final[0]
-                right_food.text = food_Final[1]
+                left_food.text = food_Final[0].Food_text.toString()!!
+                right_food.text = food_Final[1].Food_text.toString()!!
+
+                left_iamge.setImageResource(food_Final[0].Food_Image)
+                right_iamge.setImageResource(food_Final[1].Food_Image)
+
             } else {
-                Toast.makeText(this@Worldcup , left_food.text.toString() + "우승입니다!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@Worldcup,
+                    left_food.text.toString() + "우승입니다!",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-
-
         }
+
+
 
         right_card.setOnClickListener {
-            count += 2
-            game_count++
-            if (game_count <= 16) {
+                count += 2
+                game_count++
+                if (game_count <= 16 && game_count >= 2) {
 
-                left_food.text = food_1Round[count]!!
-                right_food.text = food_1Round[count + 1]!!
+                    left_food.text = food_1Round[count].Food_text.toString()!!
+                    right_food.text = food_1Round[count + 1].Food_text.toString()!!
 
-                game_count_text.text = "$game_count / 16"
+                    left_iamge.setImageResource(food_1Round[count].Food_Image)
+                    right_iamge.setImageResource(food_1Round[count + 1].Food_Image)
 
-                food_2Round.add(right_food.text.toString())
+                    game_count_text.text = "$game_count / 16"
 
-                food_2Round.shuffle()
-            } else if(game_count <= 24) {
+                    food_2Round.apply {
+                        add(
+                            Food_Data(
+                                Food_Image = food_1Round[count].Food_Image,
+                                Food_text = right_food.text.toString()
+                            )
+                        )
+                    }
 
-                count_2Round += 2
+                    food_2Round.shuffle()
+                } else if (game_count <= 24 && game_count >= 2) {
 
-                left_food.text = food_2Round[count_2Round - 2]
-                right_food.text = food_2Round[count_2Round - 1]
+                    count_2Round += 2
 
-                game_count_text.text =
-                    (game_count - 16.toInt()).toString() + " / 8\n" + food_2Round.toString() + food_2Round.size + "\n" + "3라운드 추가된거 " + food_3Round.toString() + food_3Round.size +    game_count.toString()
+                    left_food.text = food_2Round[count_2Round - 2].Food_text.toString()!!
+                    right_food.text = food_2Round[count_2Round - 1].Food_text.toString()!!
 
-                round.text = "16강"
+                    left_iamge.setImageResource(food_2Round[count_2Round - 2].Food_Image)
+                    right_iamge.setImageResource(food_2Round[count_2Round - 1].Food_Image)
 
-                food_3Round.add(right_food.text.toString())
+                    game_count_text.text =
+                        (game_count - 16.toInt()).toString() + " / 8\n"
+
+                    round.text = "16강"
+
+                    food_3Round.apply {
+                        add(
+                            Food_Data(
+                                Food_Image = food_1Round[count_2Round - 2].Food_Image,
+                                Food_text = right_food.text.toString()
+                            )
+                        )
+                    }
 
 
-                food_3Round.shuffle()
+                    food_3Round.shuffle()
 
-            } else if(game_count <= 28) {
+                } else if (game_count <= 28 && game_count >= 2) {
 
-                count_3Round += 2
+                    count_3Round += 2
 
-                left_food.text = food_3Round[count_3Round - 2]
-                right_food.text = food_3Round[count_3Round - 1]
+                    left_food.text = food_3Round[count_3Round - 2].Food_text.toString()!!
+                    right_food.text = food_3Round[count_3Round - 1].Food_text.toString()!!
 
-                game_count_text.text = (game_count - 24.toInt()).toString() + " / 4" + food_3Round.toString() + food_3Round.size + "\n" + "4강에 추가된거" + food_4Round.toString() + food_4Round.size + "게임 카운트 " + game_count.toString()
-                round.text = "8강"
+                    left_iamge.setImageResource(food_3Round[count_3Round - 2].Food_Image)
+                    right_iamge.setImageResource(food_3Round[count_3Round - 1].Food_Image)
 
-                food_4Round.add(right_food.text.toString())
 
-                food_4Round.shuffle()
+                    game_count_text.text = (game_count - 24.toInt()).toString() + " / 4"
+                    round.text = "8강"
 
-            } else if(game_count <= 30) {
+                    food_4Round.apply {
+                        add(
+                            Food_Data(
+                                Food_Image = food_3Round[count_3Round - 2].Food_Image,
+                                Food_text = right_food.text.toString()
+                            )
+                        )
+                    }
+                    food_4Round.shuffle()
 
-                count_4Round += 2
+                } else if (game_count <= 30 && game_count >= 2) {
 
-                left_food.text = food_4Round[count_4Round - 2]
-                right_food.text = food_4Round[count_4Round - 1]
+                    count_4Round += 2
 
-                game_count_text.text = (game_count - 28.toInt()).toString() + " / 4" + food_4Round.toString() + food_4Round.size + "\n" + "결승에 추가된거" + food_Final.toString() + food_Final.size + "게임 카운트 " + game_count.toString()
-                round.text = "4강"
+                    //
+                    left_food.text = food_4Round[count_4Round - 2].Food_text.toString()!!
+                    right_food.text = food_4Round[count_4Round - 1].Food_text.toString()!!
 
-                food_Final.add(right_food.text.toString())
+                    left_iamge.setImageResource(food_4Round[count_4Round - 2].Food_Image)
+                    right_iamge.setImageResource(food_4Round[count_4Round - 1].Food_Image)
 
-                food_Final.shuffle()
-            } else if(game_count <= 31) {
-                round.text = "결승전 \n"
+                    //
 
-                game_count_text.text = (game_count - 28.toInt()).toString() + " / 4" + food_Final.toString() + food_Final.size
 
-                left_food.text = food_Final[0]
-                right_food.text = food_Final[1]
-            } else {
-                Toast.makeText(this@Worldcup , right_food.text.toString() + "우승입니다!", Toast.LENGTH_SHORT).show()
+                    game_count_text.text = (game_count - 28.toInt()).toString() + " / 4"
+                    round.text = "4강"
+
+                    food_Final.apply {
+                        add(
+                            Food_Data(
+                                Food_Image = food_4Round[count_4Round - 2].Food_Image,
+                                Food_text = right_food.text.toString()
+                            )
+                        )
+                    }
+
+                    food_Final.shuffle()
+                } else if (game_count >= 31 && game_count >= 2) {
+                    round.text = "결승전 \n"
+
+                    game_count_text.text = (game_count - 28.toInt()).toString() + " / 4"
+
+                    left_food.text = food_Final[0].Food_text.toString()!!
+                    right_food.text = food_Final[1].Food_text.toString()!!
+
+                    left_iamge.setImageResource(food_Final[0].Food_Image)
+                    right_iamge.setImageResource(food_Final[1].Food_Image)
+
+                } else {
+                    Toast.makeText(
+                        this@Worldcup,
+                        left_food.text.toString() + "우승입니다!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
             }
 
-        }
 
     }
 
